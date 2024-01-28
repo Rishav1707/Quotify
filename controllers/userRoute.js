5d69b0b118f788e6593dfed60ba2e0de5e836d27
Rishav Raj <rishavraj1707music@gmail.com>
2024-01-28 22:50:38 +0530

    Remove secure true value from cookies options

--- controllers/userRoute.js
+++ controllers/userRoute.js
@@ -3,8 +3,7 @@ const bcrypt = require("bcrypt");
 const getJwtToken = require("../TokenGenerator");
 
 const options = {
-  // httpOnly: true,
-  // secure: true,
+  httpOnly: true,
   expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
   maxAge: 1000 * 60 * 60 * 24 * 7,
   // expires: Date.now() + 1000 * 60,
